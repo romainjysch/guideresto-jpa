5c4d4b3a159961612007a825f57f0968abad237b
romainjysch <rom.jys@outlook.com>
2023-04-20 21:43:43 +0200

    feat(guideresto-jpa): Préparation Hibernate pour City.java

--- src/main/java/ch/hearc/ig/guideresto/business/City.java
+++ src/main/java/ch/hearc/ig/guideresto/business/City.java
@@ -1,13 +1,28 @@
 package ch.hearc.ig.guideresto.business;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="VILLES")
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_VILLES")
+    @SequenceGenerator(name = "SEQ_VILLES", sequenceName = "SEQ_VILLES", initialValue = 1, allocationSize = 1)
+    @Column(name="NUMERO", nullable = false, length = 10)
     private Integer id;
+    @Column(name="CODE_POSTAL", nullable = false, length = 100)
     private String zipCode;
+    @Column(name="NOM_VILLE", nullable = false, length = 100)
     private String cityName;
+    @Transient
     private Set<Restaurant> restaurants;
+
+    public City() {
+
+    }
 
     public City(Integer id, String zipCode, String cityName) {
         this.id = id;
